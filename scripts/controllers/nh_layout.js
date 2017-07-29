00a286565f4dbb0ceaebef75f26f83878d0e0dd6
gongnol <979164557@qq.com>
2017-07-29 22:58:50 +0800

    no message

--- scripts/controllers/nh_layout.js
+++ scripts/controllers/nh_layout.js
@@ -104,7 +104,7 @@
             // if ($sessionStorage.appKey) {
                 var keyToBase64 = window.btoa($sessionStorage.appKey);
                 rest.userLogout(keyToBase64).then(function (resp) {
-                    if (resp.type == 'success') {
+                   /* if (resp.type == 'success') {*/
                         var idx = window.location.href.indexOf("#");
                         var currentUrl = window.location.href;
                         if (idx > -1) {
@@ -113,7 +113,7 @@
                         currentUrl += resp.data;
                         window.location.href = currentUrl;
                         window.event.returnValue = false;
-                    }
+                   /* }*/
                 })
             // }
         }
